d7eb0554a5089a94968ed9715c45e4d12423518b
aksilont <aksilont@gmail.com>
2020-12-27 14:40:35 +0300

    Initial Commit

---
+++ Avia Tickets/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Avia Tickets
+//
+//  Created by Aksilont on 27.12.2020.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Avia Tickets/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  Avia Tickets
+//
+//  Created by Aksilont on 27.12.2020.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
